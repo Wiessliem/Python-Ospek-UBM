9c8fa0ab60ac1f837b6d5a8ecf592a4cbf9d4e91
Darion Audrey <wiesenlie26@gmail.com>
2025-08-08 23:39:32 +0700

    Commit awal

---
+++ Python Ospek/Darion.py
@@ -0,0 +1,20 @@
+def main():
+    print("Mengapa saya memilih Prodi Informatika UBM?")
+    print("-" * 50)  
+    
+    alasan = [
+        "1. Prodi Informatika UBM memiliki fasilitas lengkap.",
+        "2. Prodi Informatika memiliki tenaga pengajar yang berkualitas.",
+        "3. UBM Memiliki Akreditasi Unggul",
+    ]
+    
+    for item in alasan:
+        print(item)
+    
+    print("-" * 50)
+    print("Kesimpulan:")
+    print("Prodi Informatika UBM memberikan kualitas terbaik bagi Mahasiswa/siswi "
+          "UBM untuk belajar dan berkembang.")
+
+if __name__ == "__main__":
+    main()
